62c0f864d478e8b34861569dfc6b087ce9489fef
xshyne88 <chasehomedecor@gmail.com>
2019-10-02 10:17:13 -0400

    find by lunch id

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -2,4 +2,8 @@ class Lunch < ApplicationRecord
   belongs_to :user
   belongs_to :vendor
   has_many :dishes, through: :lunch_dishes
+
+  def user(lunch)
+    User.find_by(lunch.user_id)
+  end
 end
