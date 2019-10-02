cbe0cd09fa74dc0252fd8635f23b658fae504b59
xshyne88 <chasehomedecor@gmail.com>
2019-10-02 09:26:04 -0400

    allow cors

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,16 @@ Bundler.require(*Rails.groups)
 module Lunchyard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.0#application.rb
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    config.autoload_paths << Rails.root.join("app/graphql/resolvers")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
