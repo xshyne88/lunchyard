834f49df57807657adb2397fa9ba7f7ca7166bf8
xshyne88 <chasehomedecor@gmail.com>
2019-10-02 14:26:06 -0400

    comments for organization

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,15 @@
 module Types
   class MutationType < BaseObject
+    # CREATE
     field :create_lunch, mutation: Mutations::CreateLunch
     field :create_vendor, mutation: Mutations::CreateVendor
     field :create_vendor_dish, mutation: Mutations::CreateDish
     field :add_dish_to_lunch, mutation: Mutations::AddDishToLunch
+
+    # UPDATE
+
+    # DELETE
     field :remove_dish_from_lunch, mutation: Mutations::RemoveDishFromLunch
     field :delete_lunch, mutation: Mutations::DeleteLunch
-    # TODO: updates
   end
 end
