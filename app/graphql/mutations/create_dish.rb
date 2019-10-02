205b276da08cb69df6c922a84ca47a4210e832aa
xshyne88 <chasehomedecor@gmail.com>
2019-10-02 14:26:00 -0400

    create dish fix

--- app/graphql/mutations/create_dish.rb
+++ app/graphql/mutations/create_dish.rb
@@ -2,6 +2,7 @@ module Mutations
   class CreateDish < BaseMutation
     argument :name, String, required: true
     argument :price, String, required: true
+    argument :vendor_id, String, required: true
 
     type Types::DishType
 
@@ -9,6 +10,7 @@ module Mutations
       Dish.create!({
         name: args[:name],
         price: args[:price],
+        vendor_id: args[:vendor_id],
       })
     rescue ActiveRecord::RecordInvalid => e
       GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
