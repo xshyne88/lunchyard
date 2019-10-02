3bc5346896c2bbdf28bdc0198f287cd57730a207
xshyne88 <chasehomedecor@gmail.com>
2019-10-02 10:17:25 -0400

    lunch dish type now yields dish

--- app/graphql/types/lunch_dish_type.rb
+++ app/graphql/types/lunch_dish_type.rb
@@ -1,7 +1,17 @@
 Types::LunchDishType = GraphQL::ObjectType.define do
     name 'LunchDish'
+    interfaces [GraphQL::Relay::Node.interface]
+    global_id_field :id
+
     field :lunch_id, types.ID
     field :dish_id, types.ID
     field :quantity, types.Int
+
+
+    field :dish, Types::DishType do
+      resolve -> (obj, args, ctx) {
+        Dish.find(obj.dish_id)
+      }
+    end
     # field :user, Types::UserType, null: false, resolver: Resolvers::UserResolver
   end
