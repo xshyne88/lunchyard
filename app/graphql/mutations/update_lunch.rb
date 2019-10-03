5035c43be356f5f286e71150db792b8f7e7b049e
xshyne88 <chasehomedecor@gmail.com>
2019-10-03 17:32:25 -0400

    update lunch + spec

--- app/graphql/mutations/update_lunch.rb
+++ app/graphql/mutations/update_lunch.rb
@@ -1,24 +1,43 @@
 module Mutations
+
   class UpdateLunch < BaseMutation
-    argument :occasion, String, required: true
-    argument :vendor_id, ID, required: true
-    argument :user_id, ID, required: true
+    argument :id, ID, required: true
+    argument :occasion, String, required: false
+    argument :vendor_id, ID, required: false
+    argument :user_id, ID, required: false
     argument :date, String, required: false
     argument :description, String, required: false
 
     type Types::LunchType
 
-    def resolve(args)
-      Lunch.find(args[:id])
-      Lunch.update!({
-        vendor_id: args[:vendor_id],
-        user_id: args[:user_id],
-        occasion: args[:occasion],
-        description: args[:description],
-        date: args[:date],
-      })
+    def required_fields
+      %i{id, occasion, vendor_id, date, user_id, description}.map(&:to_s)
+    end
+
+    def resolve(id:, **attributes)
+      pp attributes
+      l = Lunch.find(id)
+
+      # pp "-0----"
+      # pp args = prune_required(args)
+      l.update!(attributes)
+
+      # l.update!({
+      #   vendor_id: args[:vendor_id],
+      #   user_id: args[:user_id],
+      #   occasion: args[:occasion],
+      #   description: args[:description],
+      #   date: args[:date],
+      # })
+
+      l
+
     rescue ActiveRecord::RecordInvalid => e
       GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+    end
+
+    def prune_required(params)
+      params.slice(*required_fields).compact
     end
   end
 end
---
+++ spec/graphql/mutations/update_lunch_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+require "spec_helper"
+require "graphql"
+
+module Mutations
+  class UpdateLunch
+    RSpec.describe UpdateLunch, type: :request do
+      describe ".resolve" do
+        it "updates a lunch" do
+          expect do
+            post "/graphql", params: { query: query() }
+          end
+        end
+
+        it "returns a lunch" do
+          post "/graphql", params: { query: query() }
+          json = JSON.parse(response.body)
+          pp json
+          data = json["data"]["updateLunch"]
+
+          expect(data).to include(
+            "id" => "1",
+            "occasion" => "Birthday1",
+        )
+        end
+      end
+
+      def query()
+        <<~GQL
+          mutation {
+           updateLunch(id: 1, occasion: "Birthday1") {
+              id
+              occasion
+          }
+        }
+        GQL
+      end
+    end
+  end
+end
