6f6b0bd470e84d58e870eb4a4bdceac4558c78f6
xshyne88 <chasehomedecor@gmail.com>
2019-10-03 11:41:42 -0400

    rspec files and helpers

---
+++ spec/graphql/mutations/create_lunch_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'graphql'
+
+module Mutations
+  class CreateLunch
+    RSpec.describe CreateLunch, type: :request do
+      describe '.resolve' do
+        it 'creates a lunch' do
+          expect do
+            post '/graphql', params: { query: query() }
+          end
+        end
+
+        it 'returns a lunch' do
+          post '/graphql', params: { query: query() }
+          json = JSON.parse(response.body)
+          pp json
+          data = json['data']['createLunch']
+
+          expect(data).to include(
+            'id'           => be_present,
+            'occasion'     => 'Birthday',
+            "user"         => {'id' => "1"},
+            "vendor"       => {'id' => "1"},
+          )
+        end
+      end
+
+      def query()
+        <<~GQL
+          mutation {
+            createLunch(occasion: "Birthday", userId: 1, vendorId: 1) {
+              id
+              occasion
+              user {
+                id
+              }
+              vendor {
+               id
+              }
+            }
+          }
+        GQL
+      end
+    end
+  end
+end
