f73028249c9d13b363cd172ca0f03b320fbf9d12
xshyne88 <chasehomedecor@gmail.com>
2019-10-03 12:57:01 -0400

    create lunch spec

--- spec/graphql/mutations/create_lunch_spec.rb
+++ spec/graphql/mutations/create_lunch_spec.rb
@@ -1,28 +1,28 @@
-require 'rails_helper'
-require 'spec_helper'
-require 'graphql'
+require "rails_helper"
+require "spec_helper"
+require "graphql"
 
 module Mutations
   class CreateLunch
     RSpec.describe CreateLunch, type: :request do
-      describe '.resolve' do
-        it 'creates a lunch' do
+      describe ".resolve" do
+        it "creates a lunch" do
           expect do
-            post '/graphql', params: { query: query() }
+            post "/graphql", params: { query: query() }
           end
         end
 
-        it 'returns a lunch' do
-          post '/graphql', params: { query: query() }
+        it "returns a lunch" do
+          post "/graphql", params: { query: query() }
           json = JSON.parse(response.body)
           pp json
-          data = json['data']['createLunch']
+          data = json["data"]["createLunch"]
 
           expect(data).to include(
-            'id'           => be_present,
-            'occasion'     => 'Birthday',
-            "user"         => {'id' => "1"},
-            "vendor"       => {'id' => "1"},
+            "id" => be_present,
+            "occasion" => "Birthday",
+            "user" => { "id" => "1" },
+            "vendor" => { "id" => "1" },
           )
         end
       end
@@ -30,7 +30,7 @@ module Mutations
       def query()
         <<~GQL
           mutation {
-            createLunch(occasion: "Birthday", userId: 1, vendorId: 1) {
+           createLunch(occasion: "Birthday", userId: 1, vendorId: 1) {
               id
               occasion
               user {
@@ -39,7 +39,7 @@ module Mutations
               vendor {
                id
               }
-            }
+              }
           }
         GQL
       end
