0516079267e93619f11ea5d2020b0b976c237425
xshyne88 <chasehomedecor@gmail.com>
2019-10-08 13:26:57 -0400

    support query helpers need static module method

--- spec/support/graphql_helpers.rb
+++ spec/support/graphql_helpers.rb
@@ -1,9 +1,11 @@
 module GraphQLHelpers
-  def execute(query, variables: {})
-    LunchyardSchema.execute(
-      query,
-      variables: variables.with_indifferent_access,
-      context: {}
-    ).with_indifferent_access
+  class << self
+    def execute(query, variables: {})
+      LunchyardSchema.execute(
+        query,
+        variables: variables.with_indifferent_access,
+        context: {}
+      ).with_indifferent_access
+    end
   end
 end
