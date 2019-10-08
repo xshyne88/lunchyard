be0168f7f8d8e6c0c92549ca368c369c76cef38d
xshyne88 <chasehomedecor@gmail.com>
2019-10-08 13:27:14 -0400

    default scope for lunch model

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -2,4 +2,7 @@ class Lunch < ApplicationRecord
   belongs_to :user
   belongs_to :vendor
   has_many :dishes, through: :lunch_dishes
+
+  default_scope { order('date') }
+  scope :upcoming, -> { where("date::date > now()") }
 end
